2400547d0136fb1a85e9027ea2aae42a39a53401
Dandara Almeida <dandara.avazquez@gmail.com>
2021-04-25 10:17:16 +0200

    add attach private call on constructor

--- src/app.ts
+++ src/app.ts
@@ -46,6 +46,31 @@ function autobind(
     }; 
     return adjDescriptor;
 }
+
+// ProjectList Class
+
+class ProjectList{
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+
+
+} 
+
  
 // ProjectInput class
 class ProjectInput {
